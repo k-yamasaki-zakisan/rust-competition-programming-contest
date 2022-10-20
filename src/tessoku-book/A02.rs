cac0e5764188648d06615c72957e2c6ca9a88b69
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-10-20 09:31:52 +0900

    数列配列受け取り

---
+++ src/tessoku-book/A02.rs
@@ -0,0 +1,16 @@
+use proconio::input;
+
+fn main() {
+    input! {
+      n: i32,
+      x: i32,
+      a: [i32; n],
+    }
+    for num in a {
+      if num == x {
+        println!("Yes");
+        return
+      }
+    }
+    println!("No");
+}
