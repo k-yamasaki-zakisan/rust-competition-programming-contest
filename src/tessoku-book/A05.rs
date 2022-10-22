3bd5149c805e601d942f6dce32b9a9df14a4ff02
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-10-22 18:27:09 +0900

    ２重ループ全探索

---
+++ src/tessoku-book/A05.rs
@@ -0,0 +1,22 @@
+use proconio::input;
+
+fn main() {
+    input! {
+      nk: [i32; 2],
+    }
+
+    let n = nk[0];
+    let k = nk[1];
+    let mut ans = 0;
+    for i in 1..n+1 {
+      for j in 1..n+1 {
+        let tmp = k-(i+j);
+        if  tmp < 1 {
+          break
+        } else if tmp <= n {
+          ans += 1
+        }
+      }
+    }
+    println!("{}", ans);
+}
