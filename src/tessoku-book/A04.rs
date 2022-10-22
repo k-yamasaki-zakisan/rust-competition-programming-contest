83c06cce2a42eab98cccb1f1e0c8e43c263f682f
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-10-22 18:35:02 +0900

    バイナリ変換+0埋め

--- src/tessoku-book/A04.rs
+++ src/tessoku-book/A04.rs
@@ -1,11 +1,12 @@
 use proconio::input;
 
 // 10 -> 2進数変換
+// 0埋め(Python: zfill)
 fn main() {
     input! {
       n: i32
     }
 
     let ans = format!("{:b}", n);
-    println!("{}", ans)
+    println!("{:0>10}", ans)
 }
