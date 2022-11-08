c8d5b535a013fb510a38b6a22c3233046ae52f57
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-11-09 00:17:46 +0900

    簡単dp

---
+++ src/tessoku-book/A18.rs
@@ -0,0 +1,25 @@
+use proconio::input;
+
+// dp easy
+fn main() {
+    input! {
+        n: usize,
+        s: usize,
+        a: [usize; n],
+    }
+    let mut dp = vec![false; s + 1];
+    dp[0] = true;
+    for aa in a {
+        let mut i = s;
+        loop {
+            if dp[i] && i + aa <= s {
+                dp[i + aa] = true;
+            }
+            if i == 0 {
+                break;
+            }
+            i -= 1;
+        }
+    }
+    println!("{}", if dp[s] { "Yes" } else { "No" });
+}
