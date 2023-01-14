6afb95bb71e3ed685f6abc4aa50c32218c898622
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-01-14 17:24:19 +0900

    配列内をいい感じに処理する

--- src/tessoku-book/A60.rs
+++ src/tessoku-book/A60.rs
@@ -1,37 +1,30 @@
 use proconio::input;
-use std::collections::HashMap;
-use std::collections::VecDeque;
 
+// 配列内をいい感じに処理する
 fn main() {
     input! {
         n: usize,
         a: [usize; n],
     }
-    let mut nums_map = HashMap::new();
-    let mut cnt = 0;
-    for aa in a {
-        nums_map.entry(aa).or_insert_with(|| {
-            cnt += 1;
-            cnt
-        });
-    }
 
-    let mut bisect_array: VecDeque<usize> = VecDeque::new();
-    let mut ans = vec![-1];
-    for aa in a {
-        if bisect_array.len() == 0 {
-            bisect_array.push_front(aa);
-            continue;
-        }
-        let mut l = 0;
-        let mut r = bisect_array.len();
-        while 1 < r - l {
-            let c = (l + r) / 2;
-            if a < bisect_array[c] {
-                l = c;
+    let mut stoke: Vec<(usize, usize)> = vec![];
+    for i in 0..n {
+        while !stoke.is_empty() {
+            let (mut k, mut v) = stoke.last().unwrap();
+            if a[i] < v {
+                print!("{}", k + 1);
+                break;
             } else {
-                r = c;
+                stoke.pop();
             }
         }
+        if stoke.is_empty() {
+            print!("-1");
+        }
+        stoke.push((i, a[i]));
+        if i != n - 1 {
+            print!(" ")
+        }
     }
+    println!("");
 }
