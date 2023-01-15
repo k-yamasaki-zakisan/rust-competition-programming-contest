e5851b4791b87cd3fc5e8a61ee73b72acab868bf
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-01-15 19:23:00 +0900

    配列の文字列化

---
+++ src/tessoku-book/A61.rs
@@ -0,0 +1,23 @@
+use proconio::input;
+
+// vec -> 文字列変換（Pythonに比べてかなりめんどくさいな...）
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    }
+    let mut root = vec![vec![]; n + 1];
+    for &(a, b) in &ab {
+        root[a].push(b);
+        root[b].push(a);
+    }
+    for i in 1..=n {
+        let children = root[i]
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(", ");
+        println!("{}: {{{}}}", i, children);
+    }
+}
--- src/test.rs
+++ src/test.rs
@@ -2,3 +2,17 @@ fn main() {
     let x_int = 2.655 as i32;
     println!("{}", x_int);
 }
+
+impl Solution {
+    pub fn range_add_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut ans = vec![vec![0; n.try_into().unwrap()]; n.try_into().unwrap()];
+        for query in queries {
+            for h in query[0]..query[2] + 1 {
+                for w in query[1]..query[3] + 1 {
+                    ans[h][w] += 1
+                }
+            }
+        }
+        ans
+    }
+}
