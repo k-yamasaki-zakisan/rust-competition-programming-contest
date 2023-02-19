2d674a7d53726e578be23cb0c055393aa97a50cd
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-02-19 16:15:04 +0900

    dp

--- src/tessoku-book/B14.rs
+++ src/tessoku-book/B14.rs
@@ -1,54 +1,11 @@
 use proconio::input;
 use std::collections::HashSet;
 
-// 半分全列挙
+//
 fn main() {
     input! {
         n: usize,
         k: usize,
         a: [usize; n],
     }
-    if n <= 15 {
-        let mut a_all = HashSet::new();
-        a_all.insert(0);
-        for i in 0..n {
-            let mut tmp_all = a_all.clone();
-            for &val in &a_all {
-                tmp_all.insert(val + a[i]);
-            }
-            a_all = tmp_all;
-        }
-        if a_all.contains(&k) {
-            println!("Yes");
-            return;
-        }
-    }
-
-    let mut a_f_half = HashSet::new();
-    a_f_half.insert(0);
-    for i in 0..n / 2 {
-        let mut tmp_f_half = a_f_half.clone();
-        for &val in &a_f_half {
-            tmp_f_half.insert(val + a[i]);
-        }
-        a_f_half = tmp_f_half;
-    }
-
-    let mut a_s_half = HashSet::new();
-    a_s_half.insert(0);
-    for i in n / 2..n {
-        let mut tmp_s_half = a_s_half.clone();
-        for &val in &a_f_half {
-            tmp_s_half.insert(val + a[i]);
-        }
-        a_s_half = tmp_s_half;
-    }
-
-    for &val in &a_f_half {
-        if a_s_half.contains(&(k - val)) {
-            println!("Yes");
-            return;
-        }
-    }
-    println!("No");
 }
---
+++ src/tessoku-book/B16.rs
@@ -0,0 +1,22 @@
+use proconio::input;
+use std::collections::HashSet;
+
+// DP
+fn main() {
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+    let inf = 10000000000;
+    let mut ans = vec![inf; n];
+    ans[0] = 0;
+    for i in 0..n - 1 {
+        if i + 1 < n {
+            ans[i + 1] = ans[i + 1].min(ans[i] + (a[i] - a[i + 1]).abs())
+        }
+        if i + 2 < n {
+            ans[i + 2] = ans[i + 2].min(ans[i] + (a[i] - a[i + 2]).abs())
+        }
+    }
+    println!("{}", ans.last().unwrap());
+}
