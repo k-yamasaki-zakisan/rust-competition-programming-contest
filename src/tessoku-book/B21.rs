14ce01167d9ee0868fd844f38ea33ffeb85993d1
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-02-24 23:39:35 +0900

    dp

--- src/tessoku-book/B21.rs
+++ src/tessoku-book/B21.rs
@@ -1,10 +1,25 @@
 use proconio::input;
 use proconio::marker::Chars;
 
-// dp LCS(最長共通部分列)
+// dp
 fn main() {
     input! {
+        n: usize,
         s: Chars,
-        t: Chars,
     }
+    let mut t = vec![];
+    for i in (0..n).rev() {
+        t.push(s[i]);
+    }
+
+    let mut dp = vec![vec![0; n + 1]; n + 1];
+    for i in 1..n + 1 {
+        for j in 1..n + 1 {
+            let is_same = if s[i - 1] == t[j - 1] { 1 } else { 0 };
+            dp[i][j] = dp[i - 1][j]
+                .max(dp[i][j - 1])
+                .max(dp[i - 1][j - 1] + is_same);
+        }
+    }
+    println!("{}", dp[n][n]);
 }
