09cad23a68ac6c1dc8239509c08985f02d64fc9a
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-05-06 10:36:47 +0900

    累積和して２分探索

--- src/tessoku-book/C07.rs
+++ src/tessoku-book/C07.rs
@@ -1,5 +1,14 @@
-use proconio::input;
+use proconio::marker::Chars;
+use proconio::{input, marker::Usize1};
+use std::{
+    cmp::min, cmp::Reverse, collections::BTreeSet, collections::BinaryHeap, collections::HashMap,
+    collections::VecDeque,
+};
 
+const MOD: usize = 1000000007;
+const INF: usize = 1 << 62;
+
+// 累積和して２分探索
 fn main() {
     input! {
         n: usize,
