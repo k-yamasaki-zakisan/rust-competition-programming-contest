416eac895256746c8f670973b344d47af76732a0
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2023-06-10 23:37:20 +0900

    一旦コミット

---
+++ src/tessoku-book/C15.rs
@@ -0,0 +1,20 @@
+use proconio::marker::Chars;
+use proconio::{input, marker::Usize1};
+use std::{
+    cmp::min, cmp::Reverse, collections::BTreeSet, collections::BinaryHeap, collections::HashMap,
+    collections::VecDeque,
+};
+
+const MOD: usize = 1000000007;
+const MOD_I64: i64 = 1000000007;
+const INF: usize = 1 << 62;
+const INF_MINUS: i64 = -1000000007;
+
+//
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        lp: [(usize,usize,usize); n],
+    }
+}
